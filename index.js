8eaae0093de55eb8070e883243b125520be6e6b2
Hong <jjuggumih@gmail.com>
2020-09-16 23:02:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -62,10 +62,10 @@ async function updateGist(stats) {
     const gistContent =
         [
             ['⭐', `Total Stars`, humanize(stats.totalStars)],
-            ['➕', countAllCommits ? 'Total Commits' : 'Past Year Commits', humanize(stats.totalCommits)],
-            ['🔀', `Total PRs`, humanize(stats.totalPRs)],
+            ['☘️', countAllCommits ? 'Total Commits' : 'Past Year Commits', humanize(stats.totalCommits)],
+            ['🌊', `Total PRs`, humanize(stats.totalPRs)],
             ['🚩', `Total Issues`, humanize(stats.totalIssues)],
-            ['📦', `Contributed to`, humanize(stats.contributedTo)],
+            ['🌈', `Contributed to`, humanize(stats.contributedTo)],
         ]
             .map((content) => {
                 let line = `${content[1]}:${content[2]}`;
